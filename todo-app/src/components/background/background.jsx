99005e316a307d44fc4c5c2365e1082b8072e70b
szopian <szopiangreg@gmail.com>
2021-10-03 18:47:08 +0100

    add image to bg

--- todo-app/src/components/background/background.jsx
+++ todo-app/src/components/background/background.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import "./background.scss";
 
+import Image from "https://unsplash.com/photos/0gkw_9fy0eQ";
+
 const Background = () => {
-  return <div className="background"></div>;
+  return <div className="background" bgImage={Image}></div>;
 };
 
 export default Background;
